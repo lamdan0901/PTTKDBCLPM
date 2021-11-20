b03dea1aad803da2c1272da04da0855fcf327f5a
lamdan0901 <itmaster912@gmail.com>
2021-11-20 15:40:11 +0700

    modify script for dropdown menu

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 document.addEventListener("click", (e) => {
   const isDropdownButton = e.target.matches("[data-dropdown-button]");
-  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return;
+  const dropDowns = document.querySelectorAll("[data-dropdown]");
+
+  if (!isDropdownButton) {
+    for (let i = 0; i < dropDowns.length; i++) {
+      const openDropdown = dropDowns[i];
+      if (openDropdown.classList.contains("active")) {
+        openDropdown.classList.remove("active");
+      }
+    }
+  }
+
+  if (!isDropdownButton && dropDowns != null) return;
 
   let currentDropdown;
   if (isDropdownButton) {
@@ -13,3 +24,7 @@ document.addEventListener("click", (e) => {
     dropdown.classList.remove("active");
   });
 });
+
+//Hiện tại chưa có event cho mỗi <li></li> khi click vào,
+//chưa lấy đc data từ <li></li> đưa vào <span></span> để hiển thị
+//Và chưa có event lấy data từ bên sua-lich-chieu đưa sang để chỉnh sửa
